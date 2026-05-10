3255b40967d1ca53bb3b8351ae7113a927c04034
ruu2023 <kokihiru2038@gmail.com>
2026-05-10 13:00:51 +0900

    mock

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,11 +7,17 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get create" do
-    # 偽のGoogleレスポンスをセット
-    mock_google_auth
+    # 1. モックデータを準備
+    mock = google_oauth_mock
 
-    # Googleからのコールバックをシミュレート
+    # 2. 【重要】Rails のテスト用環境変数に OmniAuth のデータをセットする
+    # これにより、controller 内の request.env['omniauth.auth'] が nil にならなくなります
+    Rails.application.env_config["omniauth.auth"] = mock
+
+    # 3. Google からのコールバックをシミュレート
     get "/auth/google_oauth2/callback"
+
+    # 4. リダイレクト先を確認
     assert_redirected_to events_url
   end
 
