099a9742b9968b5cf0478a62e1c5f08051caf723
ruu2023 <kokihiru2038@gmail.com>
2026-05-10 13:03:08 +0900

    mock

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,28 +1,28 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
-    assert_response :success
-  end
-
   test "should get create" do
-    # 1. モックデータを準備
-    mock = google_oauth_mock
+    # 1. test_helper.rb で定義した mock_google_auth を呼び出す
+    mock = mock_google_auth
 
-    # 2. 【重要】Rails のテスト用環境変数に OmniAuth のデータをセットする
-    # これにより、controller 内の request.env['omniauth.auth'] が nil にならなくなります
+    # 2. Railsの環境変数に「これ、Googleからのデータだよ」と教え込む
+    # これを入れないと nil エラーになります
     Rails.application.env_config["omniauth.auth"] = mock
 
-    # 3. Google からのコールバックをシミュレート
+    # 3. Google認証のコールバックURLを叩く
     get "/auth/google_oauth2/callback"
 
-    # 4. リダイレクト先を確認
+    # 4. カレンダー画面（events）へ飛ばされることを確認
     assert_redirected_to events_url
+
+    # DBに正しく保存されたかチェック
+    assert_equal mock.uid, User.last.uid
   end
 
   test "should get destroy" do
-    get sessions_destroy_url
+    # ログアウト処理を叩く
+    delete logout_url
+    # トップページ（root）へリダイレクトされることを確認
     assert_redirected_to root_url
   end
 end
